7415a4ded559eb4e9ef499aaa69a504fd2a371cd
soreana <esterlinkof@gmail.com>
2016-02-28 04:09:44 +0330

    change log to error

--- ajax/subscribe.php
+++ ajax/subscribe.php
@@ -3,7 +3,7 @@
 	if( isset($_POST['email']) && !empty($_POST['email']) ){
 		
 		$email = $_POST['email'];
-		syslog(LOG_EMERG, "Email is: " + $email.PHP_EOL);
+		error_log( "Email is:" + $email.PHP_EOL);
 		
 		if( !filter_var($email, FILTER_VALIDATE_EMAIL) ){
 			die('error');
